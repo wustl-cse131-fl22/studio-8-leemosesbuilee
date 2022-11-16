597d523330e3bc3f6c830174bf6c04e0af5a558a
Ethan Moses <ethanmoses04@gmail.com>
2022-11-16 17:01:08 -0600

    final

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,60 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
 
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+	/**
+	 * Creates a date object.
+	 * @param month The month as an integer.
+	 * @param day The day as an integer.
+	 * @param year The year as an integer.
+	 * @param holiday Whether this date is a holiday.
+	 */
+	
+	public Date(int month, int day, int year, boolean holiday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holiday = holiday;
+	}
+	
+	public String toString() {
+		String result = "";
+		result = month + "/" + day + "/" + year;
+		if (this.holiday == true) {
+			result += " Holiday";
+		}
+		else {
+			result += " Not a Holiday";
+		}
+		
+		return result;
+	}
 
     public static void main(String[] args) {
-
     }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,7 +1,60 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	
+	private int hour;
+	private int minute;
+	private boolean twelveHour;
+	
+	/**
+	 * Creates a time object.
+	 * @param hour The hour (ranging from 0 to 23)
+	 * @param minute The minute (ranging from 0 to 59)
+	 * @param twelveHour Whether the time should be displayed as 12 hour or 24 hour. 
+	 */
+	
+	public Time(int hour, int minute, boolean twelveHour) {
+		this.hour = hour;
+		this.minute = minute;
+		this.twelveHour = twelveHour;
+	}
+	
+	public String toString() {
+		String time = "";
+		if (twelveHour == true) {
+			if (this.hour < 13) {
+				time = this.hour + ":" + this.minute;
+			}
+			else {
+				time = (this.hour - 12) + ":" + this.minute;
+			}
+		} 
+		else {
+			time = this.hour + ":" + this.minute;
+		}
+		
+		return time;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
 
 	public static void main(String[] args) {
  
